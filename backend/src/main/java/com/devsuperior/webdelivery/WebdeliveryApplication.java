5d04b399d266ddaccc866870557917cb6854e239
ValfridoSales <valfridosales@hotmail.com>
2024-04-02 23:25:13 -0400

    Project created

---
+++ backend/src/main/java/com/devsuperior/webdelivery/WebdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.webdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/webdelivery/WebdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.webdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
